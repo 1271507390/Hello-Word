f8eb2fc1273d4210aa606588287ff5380b885b2d
1271507390 <56377066+1271507390@users.noreply.github.com>
2019-11-02 11:31:10 +0800

    图形化设计文件
    第一次使用GitHub上传文件!

---
+++ 图形化设计.py
@@ -0,0 +1,17 @@
+
+import turtle 
+turtle.showturtle()
+turtle.write("高崎")
+turtle.forward(300)
+turtle.color("red")
+turtle.left(90)
+turtle.forward(300)
+turtle.goto(0,50)
+turtle .goto(0,0)
+turtle .penup()
+turtle.goto(0,300)
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(0,50)
+turtle.goto(50,50)
+turtle.circle(100)
